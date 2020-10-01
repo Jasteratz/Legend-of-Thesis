9f42b6d2cbebaec3b775cd84f8f605b63e3be4e2
Thomas Ratzicha <jasteratz@hotmail.com>
2020-10-01 13:54:38 +0300

    Initial Commit

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : Enemy
+
+    
+{
+    private Rigidbody2D myRigidBody;
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+    
+
+
+    // Start is called before the first frame update
+    void Start(){
+        currentState = EnemyState.idle;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player").transform;//Detect the transform of the object with the player tag
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+        CheckDistance();
+    }
+
+    void CheckDistance() {
+        if(Vector3.Distance(target.position,transform.position)<= chaseRadius //Chase the character
+                            && Vector3.Distance(target.position ,transform.position)>attackRadius)//But only while not in attack radius (prevents the collision between the center point of log and the collider of the player
+
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState!=EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                myRigidBody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+            }
+        }
+        
+        
+    }
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+            currentState = newState;
+    }
+
+    
+}
---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+    
+    //Test
+    public float delay = 0.1f;
+    public string fullText;
+    public string currentText;
+    //
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+                
+            }
+            else
+            {
+                //StartCoroutine(ShowText());
+                
+                dialogBox.SetActive(true);
+                
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+
+    //Test
+    /*IEnumerator ShowText()
+    {
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            dialogText.text= currentText;
+            yield return new WaitForSeconds(delay);
+        }
+        StartCoroutine(ShowText());
+    }*/
+}
